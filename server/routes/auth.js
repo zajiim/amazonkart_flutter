018eccc6dfc8d4a04d11e83a1e40f93c08146d82
zajiim <sajims95@gmail.com>
2022-05-31 00:59:17 +0530

    Added signin Api route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const authRouter = express.Router();
 
+//Signup route
 authRouter.post("/api/signup", async(req, res) => {
 
     try{
@@ -29,6 +31,28 @@ authRouter.post("/api/signup", async(req, res) => {
     
 });
 
+//Signin route
+
+authRouter.post('/api/signin', async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({msg: 'User with this email does not exist'});
+        }
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({msg: 'Incorrect password'});
+        }
+
+        const token = await jwt.sign({id: user._id}, "passwordKey");
+        res.json({token, ...user._doc});
+
+
+    } catch(e) {
+        res.status(500).json({error: e.message});
+    }
+});
 
 
 
